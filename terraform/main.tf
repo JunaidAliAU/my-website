81545addaf4f20f272aacab2b653dabd5131d89b
JunaidAliAU <151599630+JunaidAliAU@users.noreply.github.com>
2025-12-15 00:21:48 +0500

    Change EC2 instance type from t2.micro to t3.micro

--- terraform/main.tf
+++ terraform/main.tf
@@ -54,7 +54,7 @@ resource "aws_security_group" "flask_sg" {
 # Create EC2 Instance
 resource "aws_instance" "flask_server" {
   ami = "ami-0c55b159cbfafe1f0"  # Ubuntu 20.04 LTS (Free Tier eligible)
-  instance_type          = "t2.micro"
+  instance_type = "t3.micro"  
   key_name               = aws_key_pair.deployer_key.key_name
   vpc_security_group_ids = [aws_security_group.flask_sg.id]
   
