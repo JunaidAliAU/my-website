03a4205c3176b8819dc5a51385bf685a9387f60a
JunaidAliAU <151599630+JunaidAliAU@users.noreply.github.com>
2025-12-14 23:58:11 +0500

    Add input variables for AWS configuration
    Define input variables for AWS region and EC2 instance type.

---
+++ terraform/variables.tf
@@ -0,0 +1,13 @@
+# variables.tf - Input variables
+
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
